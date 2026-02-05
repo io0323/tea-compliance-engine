6bfedddee8a2b29177ff3f6d0df1da682ba0917b
io0323 <iokuma0323@gmail.com>
2026-02-05 15:16:39 +0900

    fix: 重複チェックロジック修正 - エンティティ作成後にexistsByLotCodeチェックを移動

--- src/main/java/com/teacompliance/service/TeaLotService.java
+++ src/main/java/com/teacompliance/service/TeaLotService.java
@@ -28,10 +28,6 @@ public class TeaLotService {
     
     @Transactional
     public TeaLot registerTeaLot(TeaLotRequest request) {
-        if (teaLotRepository.findByLotCode(request.getLotCode()).isPresent()) {
-            throw new DuplicateTeaLotException(request.getLotCode());
-        }
-        
         TeaLot teaLot = new TeaLot();
         teaLot.setLotCode(request.getLotCode());
         teaLot.setOrigin(request.getOrigin());
@@ -40,6 +36,11 @@ public class TeaLotService {
         teaLot.setPesticideLevel(request.getPesticideLevel());
         teaLot.setAromaScore(request.getAromaScore());
         teaLot.setProducedAt(request.getProducedAt());
+        
+        // Check for duplicate after creating the entity
+        if (teaLotRepository.existsByLotCode(request.getLotCode())) {
+            throw new DuplicateTeaLotException(request.getLotCode());
+        }
         
         TeaLot savedLot = teaLotRepository.save(teaLot);
         log.info("茶葉ロットを登録しました: {}", savedLot.getLotCode());
